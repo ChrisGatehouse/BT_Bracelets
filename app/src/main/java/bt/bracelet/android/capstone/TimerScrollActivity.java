54ebb5d274e587d21a4a4be645762cf271507e93
Kallen Harvey <kaharvey@pdx.edu>
2019-07-03 18:48:01 -0700

    Added Hours/Seconds to selector in TimerScrollActivity

--- app/src/main/java/bt/bracelet/android/capstone/TimerScrollActivity.java
+++ app/src/main/java/bt/bracelet/android/capstone/TimerScrollActivity.java
@@ -1,4 +1,79 @@
+/*
+ * Copyright (c) 2019, CapstoneB
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package bt.bracelet.android.capstone;
 
-public class scrollActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import no.nordicsemi.android.blinky.R;
+
+
+public class TimerScrollActivity extends Activity {
+    NumberPicker hourPicker;
+    NumberPicker minutePicker;
+    NumberPicker secondPicker;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_timer);
+
+        hourPicker = (NumberPicker)findViewById(R.id.numberPicker1);
+        minutePicker = (NumberPicker)findViewById(R.id.numberPicker2);
+        secondPicker = (NumberPicker)findViewById(R.id.numberPicker3);
+
+        textView = (TextView)findViewById(R.id.textView1);
+
+        hourPicker.setMinValue(1);
+        hourPicker.setMaxValue(12);
+        minutePicker.setMinValue(0);
+        minutePicker.setMaxValue(59);
+        secondPicker.setMinValue(0);
+        secondPicker.setMaxValue(59);
+
+        hourPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                int minutes = newVal * 60 + minutePicker.getValue();
+                int seconds = secondPicker.getValue();
+                textView.setText("Timer for " + minutes + " minutes and " + seconds + " seconds");
+            }
+        });
+        minutePicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public  void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                int minutes = hourPicker.getValue() * 60 + newVal;
+                int seconds = secondPicker.getValue();
+                textView.setText("Timer for " + minutes + " minutes and " + seconds + " seconds");
+            }
+        });
+        secondPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                int minutes = hourPicker.getValue() * 60 + minutePicker.getValue();
+                int seconds = newVal;
+                textView.setText("Timer for " + minutes + " minutes and " + seconds + " seconds");
+            }
+        });
+    }
 }
--- app/src/main/java/no/nordicsemi/android/blinky/BlinkyActivity.java
+++ app/src/main/java/no/nordicsemi/android/blinky/BlinkyActivity.java
@@ -43,79 +43,79 @@ import no.nordicsemi.android.blinky.viewmodels.BlinkyViewModel;
 
 @SuppressWarnings("ConstantConditions")
 public class BlinkyActivity extends AppCompatActivity {
-	public static final String EXTRA_DEVICE = "no.nordicsemi.android.blinky.EXTRA_DEVICE";
+    public static final String EXTRA_DEVICE = "no.nordicsemi.android.blinky.EXTRA_DEVICE";
 
-	private BlinkyViewModel mViewModel;
+    private BlinkyViewModel mViewModel;
 
-	@BindView(R.id.led_switch) Switch mLed;
-	@BindView(R.id.button_state) TextView mButtonState;
+    @BindView(R.id.led_switch) Switch mLed;
+    @BindView(R.id.button_state) TextView mButtonState;
 
-	@Override
-	protected void onCreate(final Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_blinky);
-		ButterKnife.bind(this);
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blinky);
+        ButterKnife.bind(this);
 
-		final Intent intent = getIntent();
-		final DiscoveredBluetoothDevice device = intent.getParcelableExtra(EXTRA_DEVICE);
-		final String deviceName = device.getName();
-		final String deviceAddress = device.getAddress();
+        final Intent intent = getIntent();
+        final DiscoveredBluetoothDevice device = intent.getParcelableExtra(EXTRA_DEVICE);
+        final String deviceName = device.getName();
+        final String deviceAddress = device.getAddress();
 
-		final Toolbar toolbar = findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
-		getSupportActionBar().setTitle(deviceName);
-		getSupportActionBar().setSubtitle(deviceAddress);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(deviceName);
+        getSupportActionBar().setSubtitle(deviceAddress);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-		// Configure the view model
-		mViewModel = ViewModelProviders.of(this).get(BlinkyViewModel.class);
-		mViewModel.connect(device);
+        // Configure the view model
+        mViewModel = ViewModelProviders.of(this).get(BlinkyViewModel.class);
+        mViewModel.connect(device);
 
-		// Set up views
-		final TextView ledState = findViewById(R.id.led_state);
-		final LinearLayout progressContainer = findViewById(R.id.progress_container);
-		final TextView connectionState = findViewById(R.id.connection_state);
-		final View content = findViewById(R.id.device_container);
-		final View notSupported = findViewById(R.id.not_supported);
+        // Set up views
+        final TextView ledState = findViewById(R.id.led_state);
+        final LinearLayout progressContainer = findViewById(R.id.progress_container);
+        final TextView connectionState = findViewById(R.id.connection_state);
+        final View content = findViewById(R.id.device_container);
+        final View notSupported = findViewById(R.id.not_supported);
 
-		mLed.setOnCheckedChangeListener((buttonView, isChecked) -> mViewModel.toggleLED(isChecked));
-		mViewModel.isDeviceReady().observe(this, deviceReady -> {
-			progressContainer.setVisibility(View.GONE);
-			content.setVisibility(View.VISIBLE);
-		});
-		mViewModel.getConnectionState().observe(this, text -> {
-			if (text != null) {
-				progressContainer.setVisibility(View.VISIBLE);
-				notSupported.setVisibility(View.GONE);
-				connectionState.setText(text);
-			}
-		});
-		mViewModel.isConnected().observe(this, this::onConnectionStateChanged);
-		mViewModel.isSupported().observe(this, supported -> {
-			if (!supported) {
-				progressContainer.setVisibility(View.GONE);
-				notSupported.setVisibility(View.VISIBLE);
-			}
-		});
-		mViewModel.getLEDState().observe(this, isOn -> {
-			ledState.setText(isOn ? R.string.turn_on : R.string.turn_off);
-			mLed.setChecked(isOn);
-		});
-		mViewModel.getButtonState().observe(this,
-				pressed -> mButtonState.setText(pressed ?
-						R.string.button_pressed : R.string.button_released));
-	}
+        mLed.setOnCheckedChangeListener((buttonView, isChecked) -> mViewModel.toggleLED(isChecked));
+        mViewModel.isDeviceReady().observe(this, deviceReady -> {
+            progressContainer.setVisibility(View.GONE);
+            content.setVisibility(View.VISIBLE);
+        });
+        mViewModel.getConnectionState().observe(this, text -> {
+            if (text != null) {
+                progressContainer.setVisibility(View.VISIBLE);
+                notSupported.setVisibility(View.GONE);
+                connectionState.setText(text);
+            }
+        });
+        mViewModel.isConnected().observe(this, this::onConnectionStateChanged);
+        mViewModel.isSupported().observe(this, supported -> {
+            if (!supported) {
+                progressContainer.setVisibility(View.GONE);
+                notSupported.setVisibility(View.VISIBLE);
+            }
+        });
+        mViewModel.getLEDState().observe(this, isOn -> {
+            ledState.setText(isOn ? R.string.turn_on : R.string.turn_off);
+            mLed.setChecked(isOn);
+        });
+        mViewModel.getButtonState().observe(this,
+                pressed -> mButtonState.setText(pressed ?
+                        R.string.button_pressed : R.string.button_released));
+    }
 
-	@OnClick(R.id.action_clear_cache)
-	public void onTryAgainClicked() {
-		mViewModel.reconnect();
-	}
+    @OnClick(R.id.action_clear_cache)
+    public void onTryAgainClicked() {
+        mViewModel.reconnect();
+    }
 
-	private void onConnectionStateChanged(final boolean connected) {
-		mLed.setEnabled(connected);
-		if (!connected) {
-			mLed.setChecked(false);
-			mButtonState.setText(R.string.button_unknown);
-		}
-	}
+    private void onConnectionStateChanged(final boolean connected) {
+        mLed.setEnabled(connected);
+        if (!connected) {
+            mLed.setChecked(false);
+            mButtonState.setText(R.string.button_unknown);
+        }
+    }
 }
